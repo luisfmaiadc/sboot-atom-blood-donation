de75d7ac95761ae02610dd7c01868bc8bd486af5
Luis Felipe Maia <lf.mcosta23@gmail.com>
2025-10-23 08:41:39 -0300

    Estrutura do projeto Spring boot.

---
+++ src/test/java/com/doeaqui/sboot_atom_blood_donation/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doeaqui.sboot_atom_blood_donation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
